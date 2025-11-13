51ccc769edb9f219b90f4b6648c5c4ef506e725e
DonCarla <161735866+DonCarla@users.noreply.github.com>
2025-11-13 12:57:25 +0500

    Update sofa_proxy.js

--- sofa_proxy.js
+++ sofa_proxy.js
@@ -7,16 +7,14 @@ const fetch = (...args) =>
 const app = express();
 app.use(cors());
 
-const SOFA_URL = "https://www.sofascore.com/api/v1/sport/tennis/events/live";
+const SOFA_URL = "https://sofascore.p.rapidapi.com/sport/tennis/events/live";
 
 app.get("/live", async (req, res) => {
   try {
     const response = await fetch(SOFA_URL, {
       headers: {
-        "User-Agent":
-          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.6 Safari/605.1.15",
-        "Accept": "application/json, text/plain, */*",
-        "Referer": "https://www.sofascore.com/",
+        "X-RapidAPI-Key": "491de2f94emsh815cefbc25b4a41p12296ejsn679d321c7031",
+        "X-RapidAPI-Host": "rapidapi.com",
       },
     });
 
